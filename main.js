09e18cfdbb7e6f461d4b654f7c1983b6726150d2
Minhaj Hameed <minhajham@yahoo.com>
2022-11-22 23:59:40 +0530

    playRound function added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')();
+
 
 let choices = ['rock', 'paper', 'scissors'];
 
@@ -7,7 +7,21 @@ function  getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 };
 
+function playRound(player, computer) {
+    let result = "";
 
+    if (player === computer) {
+        console.log("its a draw :(");
+    }
+    else if(player === 'rock' && computer === 'scissors' || 
+            player === 'paper' && computer === 'rock' ||
+            player === 'scissors' && computer === 'paper') {
+                result = player + " beats " + computer;
+            }
+    else {
+        result = computer + " beats " + player;
+    }        
+}
 
 
 const playerSelection = prompt('pick one tool : ','ROCK or PAPER or SCISSORS');
