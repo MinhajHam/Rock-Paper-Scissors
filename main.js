183d12cebe7bf0069c6faa01b77c341ba7aa9f65
Minhaj Hameed <minhajham@yahoo.com>
2022-12-26 14:47:23 +0530

    add pop-up window

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const container = document.querySelector(".container");
 const headContent = document.querySelector(".head-content");
 const middleContent = document.querySelector(".middle-content");
 const bottomContent = document.querySelector(".bottom-content");
-
+const modalContent = document.querySelector(".modal-content");
+const popContent = document.querySelector(".pop-content");
 
 // Middle Content Images
 const rock = document.createElement("img");
@@ -20,7 +21,6 @@ scissors.setAttribute("src", "images/scissors.png");
 scissors.classList.add("image");
 middleContent.appendChild(scissors);
 
-
 // Bottom Content
 const playerScore = document.createElement("div");
 playerScore.classList.add("score");
@@ -34,7 +34,6 @@ const computerScore = document.createElement("div");
 computerScore.classList.add("score");
 bottomContent.appendChild(computerScore);
 
-
 // Bar Div
 const statusBar = document.createElement("div");
 statusBar.classList.add("bar");
@@ -44,8 +43,7 @@ const scoreBar = document.createElement("div");
 scoreBar.classList.add("bar");
 boxContainer.appendChild(scoreBar);
 
-
-// Bar Text Div 
+// Bar Text Div
 const statusTextBar = document.createElement("div");
 statusTextBar.classList.add("bar-text");
 
@@ -55,35 +53,48 @@ const scoreTextBar = document.createElement("div");
 scoreTextBar.classList.add("bar-text");
 scoreBar.appendChild(scoreTextBar);
 
-// Score Header Div 
+// Score Header Div
 const playerName = document.createElement("div");
 playerName.classList.add("score-header");
+playerName.textContent = "Player";
 playerScore.appendChild(playerName);
 
 const computerName = document.createElement("div");
 computerName.classList.add("score-header");
+computerName.textContent = "Computer";
 computerScore.appendChild(computerName);
 
-
-// Score Text Div 
+// Score Text Div
 const playerScoreText = document.createElement("div");
 playerScoreText.classList.add("score-text");
+playerScoreText.textContent = "0";
 playerScore.appendChild(playerScoreText);
 
 const computerScoreText = document.createElement("div");
 computerScoreText.classList.add("score-text");
+computerScoreText.textContent = "0";
 computerScore.appendChild(computerScoreText);
 
+const scoreContainer = document.createElement("div");
+scoreContainer.classList.add("score-container");
+popContent.appendChild(scoreContainer);
 
+const btnContainer = document.createElement("div");
+btnContainer.classList.add("btn-container");
+popContent.appendChild(btnContainer);
 
+const cancelButton = document.createElement("button");
+cancelButton.classList.add("cancel-btn");
+cancelButton.textContent = "Try Again";
+btnContainer.appendChild(cancelButton);
 
-
-
-
-
-
+const popText = document.createElement("div");
+popText.classList.add("pop-text");
+scoreContainer.appendChild(popText);
 
 let choices = ["rock", "paper", "scissors"];
+let playerPoints = 0;
+let computerPoints = 0;
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
@@ -92,26 +103,37 @@ function getComputerChoice() {
 function playRound(player) {
   let result = "";
   let playerChoice = player.toLowerCase();
- const computerSelection = getComputerChoice();
+  const computerSelection = getComputerChoice();
 
   if (player === computerSelection) {
-    console.log("its a draw :(");
+    statusTextBar.textContent = "its a tie :(";
+    scoreTextBar.textContent = "-  -";
   } else if (
     (playerChoice === "rock" && computerSelection === "scissors") ||
     (playerChoice === "paper" && computerSelection === "rock") ||
     (playerChoice === "scissors" && computerSelection === "paper")
   ) {
-    result = "Well... you Won!! " + playerChoice + " beats " + computerSelection;
-    console.log(result);
+    playerScoreText.textContent = playerPoints += 1;
+    result = playerChoice + " beats " + computerSelection;
+    statusTextBar.textContent = result;
+    scoreTextBar.textContent = "Player Scores!!";
+
+    if (playerPoints === 5) {
+      popText.textContent = "Game Over you Won!!";
+      document.getElementById("pop-up").style.display = "block";
+    }
   } else {
-    result = "Hey... Loser!! " + computerSelection + " beats " + playerChoice;
-    console.log(result);
-  }};
+    computerScoreText.textContent = computerPoints += 1;
+    result = computerSelection + " beats " + playerChoice;
+    statusTextBar.textContent = result;
+    scoreTextBar.textContent = "Computer Scores!!";
 
-
-// let playerSelection = prompt("pick one tool : ", "rock");
-
-
+    if (computerPoints === 5) {
+      popText.textContent = "Game Over loser!!!";
+      document.getElementById("pop-up").style.display = "block";
+    }
+  }
+}
 
 rock.addEventListener("click", () => {
   playRound("rock");
@@ -124,3 +146,12 @@ paper.addEventListener("click", () => {
 scissors.addEventListener("click", () => {
   playRound("scissors");
 });
+
+// Get the modal
+var modal = document.getElementById("pop-up");
+
+
+cancelButton.addEventListener("click", () => {
+  document.getElementById("pop-up").style.display = "none";
+});
+
